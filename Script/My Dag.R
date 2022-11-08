fadd4ff0543152af4f7584d56b92063f46811cc7
meredith-meeker <mmeeker118@gmail.com>
2022-11-08 16:29:34 -0500

    two competing dags

--- Script/My Dag.R
+++ Script/My Dag.R
@@ -10,3 +10,13 @@ library(dplyr)
 
 ## Create my first dag 
 
+dag1 <- dagitty("dag{HA -> SR; HT -> SR; HT -> VH -> SR; LC -> SR; LC -> HA -> SR}")
+coordinates(dag1) <- list(x=c(G=0,P=1,H=2), y=c(G=0, P=1, H=0))
+plot(dag1)
+
+## Create my secondary dag 
+
+dag2 <- dagitty("dag{HA -> SR; HT -> SR; HT -> VH -> SR; LC -> SR; LC -> HA -> SR; LC -> VH -> SR}")
+coordinates(dag2) <- list(x=c(G=0,P=1,H=2), y=c(G=0, P=1, H=0))
+plot(dag2)
+
